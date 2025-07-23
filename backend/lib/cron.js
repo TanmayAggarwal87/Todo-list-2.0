0bce951316ffc6bdf69f330ba97ffa3c992c1fab
TanmayAggarwal87 <tanmayagg.2005@gmail.com>
2025-07-23 12:53:44 +0530

    added cron

--- backend/lib/cron.js
+++ backend/lib/cron.js
@@ -3,13 +3,13 @@ import https from "https";
 
 const job = new cron.CronJob("*/14 * * * *", function () {
   https
-    .get("todo-share.onrender.com", (res) => {
+    .get("https://todo-share.onrender.com", (res) => {
       if (res.statusCode === 200) console.log("GET request sent successfully");
       else console.log("GET request failed", res.statusCode);
     })
     .on("error", (e) => console.error("Error while sending request", e));
 });
-
+job.start()
 export default job;
 
 // CRON JOB EXPLANATION:
